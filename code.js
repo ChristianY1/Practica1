f57b5bfb3578abe11803a50fcbe974510b30810b
ChristianY1 <christianyunga@gmail.com>
2020-04-09 05:20:18 -0500

    paginacion

--- code.js
+++ code.js
@@ -19,21 +19,30 @@ function buscarPeliculaPorTitulo(){
         xmlhttp.onreadystatechange = function(){
             if (this.readyState == 4 && this.status == 200) {
                 var data = JSON.parse(this.responseText)
-                //console.log(data);
+                //console.log(datdata.Search.movie);
+                var vector = []
                 data.Search.forEach(movie => {
-                    detalles +="<td>" + 
-                                        "<tr><img src= "+ movie.Poster + " width=150  ></tr>" + 
-                                        "<tr>" + movie.Title + "</tr>" + 
-                                        "<tr>" + movie.Year + "</tr>" +
-                                        "<tr>" + movie.Type + "</tr>"+
-                                        "<tr>"+ "<input type = button onclick =\" buscarPeliculaPorID('"+ movie.imdbID +"')\" value = ver mas >" +"</tr>"
-                                        
-                               " <td>";
-             
+                vector.push ([movie.Poster,movie.Title,movie.Year,movie.Type,movie.imdbID])                       
                     
                 });
+                console.log(vector)
+                console.log(vector.length)
+                for (var i = 0; i < 5; i++) {
+                    detalles +="<td>" + 
+                    "<tr><img src= "+ vector[i][0] + " width=150  ></tr>" + 
+                    "<tr>" + vector[i][1] + "</tr>" + 
+                    "<tr>" + vector[i][2] + "</tr>" +
+                    "<tr>" + vector[i][3] + "</tr>"+
+                    "<tr>"+ "<input type = button onclick =\" buscarPeliculaPorID('"+ vector[i][4]+"')\" value = ver mas >" +"</tr>"
+                    "<td>"+"<br>";
+                   
+                    
+                }
                 
-               
+                       
+                   
+                
+                document.getElementById("detalles").innerHTML = " ";
                 document.getElementById("informacion").innerHTML = detalles;
             }
         };
@@ -122,6 +131,62 @@ function buscarPeliculaPorID2(){
     xmlhttp.open("GET","http://www.omdbapi.com/?i="+idPelicula+"&apikey=d358b412",true);
     xmlhttp.send();
   
+}
+
+function segundaParte(){
+  
+    var titulo = document.getElementById("titulo").value
+    
+        
+    var detalles = "";
+    if(titulo == ""){
+        detalles = "<tr>" + 
+        "<td colspan = '5'> "+"Sin información disponible.."+"</td>"+
+        "</tr>";
+        document.getElementById("informacion").innerHTML = detalles;
+    }else{
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            
+        }
+        xmlhttp.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.responseText)
+                //console.log(datdata.Search.movie);
+                var vector = []
+                data.Search.forEach(movie => {
+                vector.push ([movie.Poster,movie.Title,movie.Year,movie.Type,movie.imdbID])                       
+                    
+                });
+                console.log(vector)
+                console.log(vector.length)
+                for (var i = 5; i < vector.length; i++) {
+                    detalles +="<td>" + 
+                    "<tr><img src= "+ vector[i][0] + " width=150  ></tr>" + 
+                    "<tr>" + vector[i][1] + "</tr>" + 
+                    "<tr>" + vector[i][2] + "</tr>" +
+                    "<tr>" + vector[i][3] + "</tr>"+
+                    "<tr>"+ "<input type = button onclick =\" buscarPeliculaPorID('"+ vector[i][4]+"')\" value = ver mas >" +"</tr>"
+                    "<td>"+"<br>";
+                   
+                    
+                }
+                
+                       
+                   
+                
+                document.getElementById("detalles").innerHTML = " ";
+                document.getElementById("informacion").innerHTML = detalles;
+            }
+        };
+        
+        xmlhttp.open("GET","http://www.omdbapi.com/?i=tt3896198&apikey=d358b412&s="+ titulo + "&plot=full",true);
+        xmlhttp.send();
+       
+        
+    }
 }
 
 
